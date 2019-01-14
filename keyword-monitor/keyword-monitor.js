3df23ebf7f489c0590f5549e488a6e8ac2aa85f6
Gabriel de Oliveira  Barbosa <manobi.oliveira@gmail.com>
2019-01-13 22:17:56 -0200

    refactoring keyword job

--- keyword-monitor/keyword-monitor.js
+++ keyword-monitor/keyword-monitor.js
@@ -1,26 +1,26 @@
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
+const google = 'https://www.google.com';
 
-module.exports = (ctx, done) => {
-    const { keyword, competitors } = ctx.task.data;
-    return JSDOM.fromURL(`https://www.google.com/search?q=${keyword.split(' ').join('+')}`).then(dom => {
-        const { window } = dom,
-            { document } = window;
-
-        const links = [
+module.exports = async (ctx, done) => {
+    // 1. Task monitoring data
+    const { keyword, competitors } = ctx.task.data,
+        q = keyword.split(' ').join('+');
+    
+    // 2. Load and parse search result page
+    const dom = await JSDOM.fromURL(`${google}/search?q=${q}`),
+        { window } = dom,
+        { document } = window;
+        links = [
             ...document.querySelectorAll('.g .r > a')
         ].map((entry) => {
-            const link = entry.getAttribute('href'),
-            url = link.substring(7, (link.length - 1));
-            return url;
-        });
-    
-        const result = competitors.map((competitor) => {
-            return {
-                [competitor]: links.findIndex(link => link.startsWith(competitor))
-            }
+            return entry.getAttribute('href').substring(7, (link.length - 1));
         });
 
-        done(null, result);
+    // 3. Return competitors index
+    return competitors.map((competitor) => {
+        return {
+            [competitor]: links.findIndex(link => link.startsWith(competitor))
+        }
     });
 }
